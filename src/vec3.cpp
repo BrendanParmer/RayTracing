72be7eeb4939612b96573f1c81371ee5fc2eac38
Brendan Parmer <bjparmer@sbcglobal.net>
2021-09-11 22:42:27 -0500

    Fixed vec3 arithmetic operators not loading outside of vec3.cpp

--- src/vec3.cpp
+++ src/vec3.cpp
@@ -1,46 +1,24 @@
 #include "vec3.h"
 
+vec3::vec3()
+{
+    x = 0;
+    y = 0;
+    z = 0;
+}
+
 vec3::vec3(double set_x, double set_y, double set_z)
 {
     x = set_x;
     y = set_y;
     z = set_z;
 }
+
+
 vec3 vec3::operator-() const {
     return vec3(-x, -y, -z);
 }
 
-vec3 vec3::operator+(const vec3 &other)
-{
-    return vec3(x+other.x, y+other.y, z+other.z);
-}
-
-vec3 vec3::operator-(const vec3 &other)
-{
-    return (*this) + -other;
-}
-
-vec3 vec3::operator*(const double &a)
-{
-    return vec3(a*x, a*y, a*z);
-}
-
-vec3 vec3::operator/(const double &a)
-{
-    return vec3(x/a, y/a, z/a);
-}
-
-double vec3::dot(const vec3 &u, const vec3 &v)
-{
-    return u.x*v.x + u.y*v.y + u.z*v.z;
-}
-
-vec3 vec3::cross(const vec3 &u, const vec3 &v)
-{
-    return vec3(u.y*v.z - u.z*v.y,
-                u.z*v.x - u.x*v.z,
-                u.x*v.y - u.y*v.x);
-}
 
 vec3 vec3::operator+=(const vec3 &other)
 {
@@ -54,13 +32,14 @@ vec3 vec3::operator-=(const vec3 &other)
 
 vec3 vec3::operator*=(const double &a)
 {
-    return *this * a;
+    return a * (*this);
 }
 
 vec3 vec3::operator/=(const double &a)
 {
-    return *this/a;
+    return (*this) / a;
 }
+
 
 double vec3::length() const {
     return std::sqrt(x*x + y*y + z*z);
@@ -77,6 +56,7 @@ std::string vec3::to_string(const vec3 &v)
                + std::to_string(v.y) + ", " 
                + std::to_string(v.z) + ")";
 }
+
 
 double vec3::operator[](int i) const {
     i %= 3;
--- src/vec3.h
+++ src/vec3.h
@@ -9,19 +9,11 @@ class vec3 {
         double y;
         double z;
 
+        vec3();
         vec3(double x, double y, double z);
 
         //negative
         vec3 operator-() const;
-
-        //arithmetic
-        vec3 operator+(const vec3 &other);
-        vec3 operator-(const vec3 &other);
-        vec3 operator*(const double &a);
-        vec3 operator/(const double &a);
-
-        double dot(const vec3 &u, const vec3 &v);
-        vec3 cross(const vec3 &u, const vec3 &v);
 
         vec3 operator+=(const vec3& other);
         vec3 operator-=(const vec3& other);
@@ -37,6 +29,44 @@ class vec3 {
         double operator[](int i) const;
         double operator[](unsigned i) const;
 };
+
+inline vec3 operator+(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.x+v.x, u.y+v.y, u.z+v.z);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.x-v.x, u.y-v.y, u.z-v.z);
+}
+
+inline vec3 operator*(double t, const vec3 &v)
+{
+    return vec3(t*v.x, t*v.y, t*v.z);
+}
+
+inline vec3 operator*(const vec3 &v, double t)
+{
+    return t * v;
+}
+
+inline vec3 operator/(const vec3 &v, const double &t)
+{
+    return vec3(v.x/t, v.y/t, v.z/t);
+}
+
+
+inline double dot(const vec3 &u, const vec3 &v)
+{
+    return u.x*v.x + u.y*v.y + u.z*v.z;
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.y*v.z - u.z*v.y,
+                u.z*v.x - u.x*v.z,
+                u.x*v.y - u.y*v.x);
+}
 
 using point3 = vec3;
 using color = vec3;
